724a18774dc27af4227a856274cebed64eca2a95
Nxtra <nickvanhoof1@gmail.com>
2018-03-10 15:55:48 +0100

    Add mappings for registration and login view

--- Wisdom/src/main/java/wisdom/Controllers/HomeController.java
+++ Wisdom/src/main/java/wisdom/Controllers/HomeController.java
@@ -3,6 +3,7 @@ package wisdom.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,5 +13,15 @@ public class HomeController {
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "there") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
     }
 }
